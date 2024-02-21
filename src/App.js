6f043acaf5f318199450364aa2245c8740d98609
Lenilson Lima Pantoja <lenilsonlm.pantoja@gmail.com>
2024-02-21 16:13:05 -0400

    21-02-2024

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
-import RelatorioPdf from './pages/home/relatorio/RelatorioPdf';
 
 function App() {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
-      <Route path="/relatorio-pdf" element={<RelatorioPdf />} />
     </Routes>
   );
 }
